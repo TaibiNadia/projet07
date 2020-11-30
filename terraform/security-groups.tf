7b3547817dbd740635f0a499fd7846ab62fc342d
TaibiNadia <46710778+TaibiNadia@users.noreply.github.com>
2020-11-30 18:49:52 +0100

    Update security-groups.tf

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,80 +1,162 @@
-Requirements:
-Terraform
-Ansible
-AWS
-The purpose is to automate the creation of the infrastructure and the setup of the latest wordpress application, creating a fully operational AWS VPC network (subnets, routing tables, igw, etc), it will also create everything that need to be for creating EC2 and RDS instances (security key, security group, subnet group).
+resource "aws_security_group" "bastion_SG" {
+  vpc_id       = aws_vpc.main_vpc.id
+  name         = "Bastion Security Group"
+  description  = "Bastion Security Group"
 
-It will also create the Elastic Load Balancer and add the EC2 instance(s) automatically that were created using ansible playbook.
+  tags = {
+    Name = "Bastion_SG"
+  }
+}
 
-Resources
-Create a VPC with 4x VPC subnets (2x public and 2x private) in different AZ zones inside the AWS region
-Create one specific security group only acesible from ec2 instances and not from public load balancing dns address 22
-Create one specific security group for ec2 from bastion and load balancer
-Create one specific security group for rds 3306 (not public internet facing, only from vpc)
-Create one specific security group for load balancer on port 80
-Provision ec2 instances with default ubuntu ami in 2 different public AZ
-Provision one RDS instance in private subnets
-Launch and configure public facing VPC LB (cross_az_load_balancing) and attach VPC subnets
-Register EC2 instances on LB
+resource "aws_security_group_rule" "http_to_bastion_ingress_rule" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_SG.id
+}
+resource "aws_security_group_rule" "bastion_to_web_egress_rule" {
+  type                       = "egress"
+  from_port                  = 22
+  to_port                    = 22
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.web_SG.id
+  security_group_id          = aws_security_group.bastion_SG.id
+}
+resource "aws_security_group_rule" "bastion_to_db_egress_rule" {
+  type                       = "egress"
+  from_port                  = 22
+  to_port                    = 22
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.db_SG.id
+  security_group_id          = aws_security_group.bastion_SG.id
+}
 
-Tools used
+resource "aws_security_group" "web_SG" {
+  vpc_id       = aws_vpc.main_vpc.id
+  name         = "web Security Group"
+  description  = "web Security Group"
 
-ansible --version
-ansible 2.9.14
-terraform version
-Terraform v0.12.24
-Before using the terraform, we need to export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY as environment variables:
+  tags = {
+    Name = "Web_SG"
+  }
+}
+resource "aws_security_group_rule" "lb_to_web_ingress_rule" {
+  type                       = "ingress"
+  from_port                  = 80
+  to_port                    = 80
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.loadbalancer_SG.id
+  security_group_id          = aws_security_group.web_SG.id
+}
+resource "aws_security_group_rule" "bastion_to_web_ingress_rule" {
+  type                       = "ingress"
+  from_port                  = 22
+  to_port                    = 22
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.bastion_SG.id
+  security_group_id          = aws_security_group.web_SG.id
+}
+resource "aws_security_group_rule" "web_to_db_egress_rule" {
+  type                       = "egress"
+  from_port                  = 3306
+  to_port                    = 3306
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.db_SG.id
+  security_group_id          = aws_security_group.web_SG.id
+}
 
-export AWS_ACCESS_KEY_ID="xxxxxxxxxxxxxxxx"
-export AWS_SECRET_ACCESS_KEY="yyyyyyyyyyyyyyyyyyyy"
+resource "aws_security_group_rule" "web_to_http_egress_rule" {
+  type                       = "egress"
+  from_port                  = 80
+  to_port                    = 80
+  protocol                   = "tcp"
+  cidr_blocks                = ["0.0.0.0/0"]
+  security_group_id          = aws_security_group.web_SG.id
+}
+resource "aws_security_group_rule" "web_to_bastion_egress_rule" {
+  type                       = "egress"
+  from_port                  = 22
+  to_port                    = 22
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.bastion_SG.id
+  security_group_id          = aws_security_group.web_SG.id
+}
 
-or use it with AWS CLI configuration.
+resource "aws_security_group" "db_SG" {
+  vpc_id       = aws_vpc.main_vpc.id
+  name         = "db Security Group"
+  description  = "db Security Group"
 
-1. Infrastructure
-Plan
-Initialize terraform: cd terraform-aws terraform init
+  tags = {
+    Name = "DB_SG"
+  }
+}
+resource "aws_security_group_rule" "web_to_db_ingress_rule" {
+  type                       = "ingress"
+  from_port                  = 3306
+  to_port                    = 3306
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.web_SG.id
+  security_group_id          = aws_security_group.db_SG.id
+}
+resource "aws_security_group_rule" "bastion_to_db_ingress_rule" {
+  type                       = "ingress"
+  from_port                  = 22
+  to_port                    = 22
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.bastion_SG.id
+  security_group_id          = aws_security_group.db_SG.id
+}
 
-Check config variables at terraform-aws/variables.yml
+resource "aws_security_group_rule" "db_to_web_egress_rule" {
+  type                       = "egress"
+  from_port                  = 3306
+  to_port                    = 3306
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.web_SG.id
+  security_group_id          = aws_security_group.db_SG.id
+}
+resource "aws_security_group_rule" "db_to_bastion_egress_rule" {
+  type                       = "egress"
+  from_port                  = 22
+  to_port                    = 22
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.bastion_SG.id
+  security_group_id          = aws_security_group.db_SG.id
+}
+resource "aws_security_group" "loadbalancer_SG" {
+  vpc_id       = aws_vpc.main_vpc.id
+  name         = "loadbalancer Security Group"
+  description  = "loadbalancer Security Group"
 
-To generate and show an execution plan (dry run): terraform plan
+  tags = {
+    Name = "LB_SG"
+  }
+}
+resource "aws_security_group_rule" "http_to_lb_ingress_rule" {
+  type                       = "ingress"
+  from_port                  = 80
+  to_port                    = 80
+  protocol                   = "tcp"
+  cidr_blocks                = ["0.0.0.0/0"]
+  security_group_id          = aws_security_group.loadbalancer_SG.id
+}
 
-You can select one of ubuntu images from Ubuntu Amazon EC2 AMI Locator, based on your aws region and edit in terraform-aws/variables.yml
-Deploy
-To build or makes actual changes in infrastructure: terraform apply
-
-To inspect Terraform state or plan: terraform show
-
-To destroy Terraform-managed infrastructure: terraform destroy
-
-Note: Terraform stores the state of the managed infrastructure from the last time Terraform was run. Terraform uses the state to create plans and make changes to the infrastructure.
-
-Once the Terraform create all the resources over AWS, it will use ansible to install the wordpress over the EC2 instance(s).
-
-Playbook
-
-# change database settings in role/wordpress/defaults/main.yml
-db_hostname: "instance_address"    A recupere de l'instance RDS
-db_name: "mybd"
-db_username: "foo"
-db_password: "hypersecret"
-
-#ip address of EC2  in inventory file
-@ip instance web 
-
-# configuration in ansible.configuration with path to the ssh key and the user
-inventory = ./inventory
-private_key_file = ../key_pair
-remote_user = ubuntu
-
-Ansible
-
-Play-book:
-Install the nginx
-nginx.yml
-Install PHP7.2-FPM with modules
-php7.yml
-Install wordpress
-deploy.yml
-
-ansible-playbook site.yml --private-key=path/to/key -u ubuntu
-
+resource "aws_security_group_rule" "bastion_to_lb_ingress_rule" {
+  type                       = "ingress"
+  from_port                  = 22
+  to_port                    = 22
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.bastion_SG.id
+  security_group_id          = aws_security_group.loadbalancer_SG.id
+}
+resource "aws_security_group_rule" "lb_to_web_egress_rule" {
+  type                       = "egress"
+  from_port                  = 80
+  to_port                    = 80
+  protocol                   = "tcp"
+  source_security_group_id   = aws_security_group.web_SG.id
+  security_group_id          = aws_security_group.loadbalancer_SG.id
+}
